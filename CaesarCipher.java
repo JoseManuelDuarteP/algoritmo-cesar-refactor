0f2b7c00e4d670d44ab63da3a29a72d054ce51d0
JoseManuelDuarteP <josduaper@alu.edu.gva.es>
2026-03-26 15:30:47 +0100

    Refactor Jose

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -12,108 +12,65 @@ Tu tarea consiste en entender el código y refactorizarlo para que sea lo más l
 según lo visto en la sesión de Clean Code
 */
 
+import static java.lang.Character.*;
+
 public class CaesarCipher {
     
     private static final int ALPHABET_LENGTH = 26;
-    
-    private static class LetterASCIICodes {
-        static final int A = 65;
-        static final int Z = 90;
-        static final int a = 97;
-        static final int z = 122;
+
+    private static char spaceHandler(char currentChar, int cipherDisplacement) {
+        return (char) (currentChar + cipherDisplacement);
     }
 
-    private static boolean isUpperCaseLetterOutOfRange(int charCode, int cipherDisplacement) {
-        if (charCode >= LetterASCIICodes.A && charCode <= LetterASCIICodes.Z) {
-
-            if (charCode + cipherDisplacement > LetterASCIICodes.Z || charCode - cipherDisplacement < LetterASCIICodes.A) {
-                return true;
-            }
-            return false;
-
-        }
-        return true;
-    }
-    
-    private static boolean isLowerCaseOutOfRange(int charCode, int cipherDisplacement) {
-        if (charCode >= LetterASCIICodes.a && charCode <= LetterASCIICodes.z) {
-
-            if (charCode + cipherDisplacement > LetterASCIICodes.z || charCode - cipherDisplacement < LetterASCIICodes.a) {
-                return true;
-            }
-            return false;
-
-        }
-        return true;
-    }
-    
-    private static boolean isOutOfAlphabet(int charCode, int cipherDisplacement) {
-        return isUpperCaseLetterOutOfRange(charCode, cipherDisplacement) ||
-               isLowerCaseOutOfRange(charCode, cipherDisplacement);
-    }
-
-    public static String cipher(String text, int cipherDisplacement) {
+    public static String resolver(String text, int cipherDisplacement) {
         StringBuilder cipher = new StringBuilder();
-        char cipheredLetter;
-        int displacementToApply, currentChar;
+        char cipheredLetter, currentChar;
         cipherDisplacement = cipherDisplacement % ALPHABET_LENGTH;
-        
+
         for (int i = 0; i < text.length(); i++) {
             currentChar = text.charAt(i);
 
-            if (isOutOfAlphabet(currentChar, cipherDisplacement)) {
-                displacementToApply = cipherDisplacement - ALPHABET_LENGTH;
-            } else {
-                displacementToApply = cipherDisplacement;
+            if (isSpaceChar(currentChar) || isSpaceChar(currentChar + cipherDisplacement)) {
+                cipher.append(spaceHandler(currentChar, cipherDisplacement));
+                continue;
             }
 
-            cipheredLetter = (char) (currentChar + displacementToApply);
+            cipheredLetter = (char) (currentChar + cipherDisplacement);
             cipher.append(cipheredLetter);
         }
         return cipher.toString();
     }
-    
-    public static String decipher(String text, int cipherDisplacement) {
-        StringBuilder decipher = new StringBuilder();
-        char cipheredLetter;
-        int displacementToApply, currentChar;
-        cipherDisplacement = -cipherDisplacement % ALPHABET_LENGTH;
-        
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = text.charAt(i);
 
-            if (isOutOfAlphabet(currentChar, cipherDisplacement)) {
-                displacementToApply = cipherDisplacement + ALPHABET_LENGTH;
-            } else {
-                displacementToApply = cipherDisplacement;
-            }
-
-            cipheredLetter = (char) (currentChar + displacementToApply);
-            decipher.append(cipheredLetter);
-        }
-        return decipher.toString();
-    }
-
-    /* Quería comentar que viendo que tanto cifrar como descifrar tienen una lógica idéntica solo
-    diferenciada por el operador del for se me ocurrió sacar el for a otro método que operara de x
-    manera según un parámetro que se le pasara. Pero sabiendo que este tipo de cifrado sería muy
-    raro que cambiara me pareció un nivel innecesario de abstracción.
-    */
-    
     public static void main(String[] args) {
         // Test 1
-        String result1 = cipher("Hello World", 1);
+        String result1 = resolver("Hello World", 1);
         String expected1 = "Ifmmp!Xpsme";
-        assert result1.equals(expected1) : 
-            String.format("%s === '%s'", result1, expected1);
-        
+        if (!result1.equals(expected1)) {
+            throw new RuntimeException(
+                    String.format("Test 1 FAILED: '%s' !== '%s'", result1, expected1)
+            );
+        }
+
         // Test 2
-        String ciphered = cipher("Hello World", 3);
-        String result2 = decipher(ciphered, 3);
+        String ciphered2 = resolver("Hello World", 3);
+        String result2 = resolver(ciphered2, -3);
         String expected2 = "Hello World";
-        assert result2.equals(expected2) : 
-            String.format("%s === '%s'", result2, expected2);
-        
+        if (!result2.equals(expected2)) {
+            throw new RuntimeException(
+                    String.format("Test 2 FAILED: '%s' !== '%s'", result2, expected2)
+            );
+        }
+        // Test 3
+        String ciphered3 = resolver("¡Hola que tal...!", 28);
+        String result3 = resolver(ciphered3, -28);
+        String expected3 = "¡Hola que tal...!";
+        if (!result3.equals(expected3)) {
+            throw new RuntimeException(
+                    String.format("Test 3 FAILED: '%s' !== '%s'", result3, expected3)
+            );
+        }
+
+
         System.out.println("Todos los tests han pasado correctamente");
     }
 }
